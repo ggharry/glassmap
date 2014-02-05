90e86e89b6f9758dc0d6cc83465e3cc64bcf90d8
Harry Chen <harrychenca@gmail.com>
2014-02-05 14:44:26 -0500

    added arkin's code

---
+++ arduino/harry_code_feb_3/harry_code_feb_3.ino
@@ -0,0 +1,17 @@
+#include <SoftwareSerial.h>
+
+
+SoftwareSerial GPS = SoftwareSerial(0,2);
+
+void setup(){
+//  Serial.begin(9600);
+//  Serial.println("Start");
+//  
+//  GPS.begin(4800);
+  
+}
+
+void loop(){
+//  boolean avail = GPS.available();
+//  Serial.println(avail);
+}
